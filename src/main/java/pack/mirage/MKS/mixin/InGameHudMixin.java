ced6ccbdd48e8abac54cb69227c7c42193110290
Ennui Langeweile <85590273+EnnuiL@users.noreply.github.com>
2024-02-27 23:57:33 -0300

    More day counter shenanigans

--- src/main/java/pack/mirage/MKS/mixin/InGameHudMixin.java
+++ src/main/java/pack/mirage/MKS/mixin/InGameHudMixin.java
@@ -7,7 +7,7 @@ import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.text.Text;
-import net.minecraft.util.Colors;
+import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -35,7 +35,9 @@ public abstract class InGameHudMixin {
     @Unique
     private int dayCounterTimerFade;
 
-    // TODO - make this Not Permanent
+    @Unique
+    private int dayCount;
+
     @Inject(
             method = "render",
             at = @At(
@@ -44,14 +46,51 @@ public abstract class InGameHudMixin {
             )
     )
     private void renderStuff(DrawContext context, float tickDelta, CallbackInfo ci) {
-        context.getMatrices().push();
-        context.drawCenteredTextWithShadow(
-                this.getTextRenderer(),
-                Text.translatable("hud.mirage-kitchens-sink.day_counter", this.client.world.getTimeOfDay() / 24000L),
-                this.scaledWidth / 2,
-                this.scaledHeight - 48,
-                Colors.WHITE
-        );
-        context.getMatrices().pop();
+        if (this.dayCounterTimerFade > 0) {
+            float timeLeft = this.dayCounterTimerFade - tickDelta;
+            int alpha = 255;
+
+            if (this.dayCounterTimerFade > 20 + 100) {
+                float progress = (20.0F + 100.0F + 20.0F) - timeLeft;
+                alpha = (int) ((progress * 255.0F) / 20.0F);
+            }
+
+            if (this.dayCounterTimerFade <= 20) {
+                alpha = (int) ((timeLeft * 255.0F) / 20.0F);
+            }
+
+            alpha = MathHelper.clamp(alpha, 0, 255);
+
+            if (alpha > 8) {
+                context.getMatrices().push();
+                context.drawCenteredTextWithShadow(
+                        this.getTextRenderer(),
+                        Text.translatable("hud.mirage-kitchens-sink.day_counter", this.client.world.getTimeOfDay() / 24000L),
+                        this.scaledWidth / 2,
+                        this.scaledHeight - 48,
+                        0x00FFFFFF | (alpha << 24 & 0xFF000000)
+                );
+                context.getMatrices().pop();
+            }
+        }
+    }
+
+    // TODO - perhaps find a more optimal way of doing this
+    @Inject(method = "tick()V", at = @At("TAIL"))
+    private void tickStuff(CallbackInfo ci) {
+        if (this.client.world != null) {
+            if (this.dayCounterTimerFade > 0) {
+                this.dayCounterTimerFade--;
+            } else {
+                if ((int) (this.client.world.getTimeOfDay() / 24000L) != this.dayCount) {
+                    this.dayCount = (int) (this.client.world.getTimeOfDay() / 24000L);
+                    if (this.dayCount % 5 == 0) {
+                        this.dayCounterTimerFade = 20 + 100 + 20;
+                    }
+                }
+            }
+        } else {
+            this.dayCount = -1;
+        }
     }
 }
