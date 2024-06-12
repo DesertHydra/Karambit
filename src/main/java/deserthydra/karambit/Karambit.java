b8b5daf270e0c531a39b4ae7141b1835eb6e15a0
maverik <mavrixen@gmail.com>
2024-06-12 13:28:17 -0300

    json ? aw man

--- src/main/java/deserthydra/karambit/Karambit.java
+++ src/main/java/deserthydra/karambit/Karambit.java
@@ -5,7 +5,11 @@ import deserthydra.karambit.registry.BoatItems;
 import deserthydra.karambit.registry.CreativeTabOrder;
 import deserthydra.karambit.registry.RosewaterBlocks;
 import deserthydra.karambit.registry.RosewaterItems;
+import net.minecraft.resource.ResourceFinder;
+
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,6 +18,11 @@ import org.slf4j.LoggerFactory;
 public class Karambit implements ModInitializer {
 	public static final String MOD_ID = "karambit";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@NotNull
+	public static Identifier id(@NotNull String path) {
+		return new Identifier(MOD_ID, path);
+	}
 
 	@Override
 	public void onInitialize() {
--- src/main/java/deserthydra/karambit/registry/RosewaterBlocks.java
+++ src/main/java/deserthydra/karambit/registry/RosewaterBlocks.java
@@ -1,17 +1,23 @@
 package deserthydra.karambit.registry;
 
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.type.BlockSetTypeBuilder;
+import net.fabricmc.fabric.api.object.builder.v1.block.type.WoodTypeBuilder;
 import net.fabricmc.fabric.api.registry.StrippableBlockRegistry;
 import net.minecraft.block.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
+
 import static deserthydra.karambit.Karambit.MOD_ID;
+import static deserthydra.karambit.Karambit.id;
 import static net.minecraft.block.Blocks.*;
 
 public class RosewaterBlocks {
 
+    public static final BlockSetType ROSEWATER_SET = BlockSetTypeBuilder.copyOf(BlockSetType.CRIMSON).register(id("rosewater"));
+    public static final WoodType ROSEWATER_WOOD_TYPE = WoodTypeBuilder.copyOf(WoodType.CRIMSON).register(id("rosewater"), ROSEWATER_SET);
 
     public static final Block ROSEWATER_STEM = register("rosewater_stem", new PillarBlock(FabricBlockSettings.copyOf(CRIMSON_STEM).mapColor(MapColor.PINK)));
     public static final Block ROSEWATER_HYPHAE = register("rosewater_hyphae", new PillarBlock(FabricBlockSettings.copyOf(CRIMSON_HYPHAE).mapColor(MapColor.PINK)));
@@ -24,15 +30,16 @@ public class RosewaterBlocks {
     public static final Block ROSEWATER_SLAB = register("rosewater_slab", new SlabBlock(FabricBlockSettings.copyOf(CRIMSON_SLAB)));
 
     public static final Block ROSEWATER_FENCE = register("rosewater_fence", new FenceBlock(FabricBlockSettings.copyOf(CRIMSON_FENCE)));
-    public static final Block ROSEWATER_FENCE_GATE = register("rosewater_fence_gate", new FenceGateBlock(FabricBlockSettings.copyOf(CRIMSON_FENCE_GATE).mapColor(MapColor.PINK), WoodType.CRIMSON));
+    public static final Block ROSEWATER_FENCE_GATE = register("rosewater_fence_gate", new FenceGateBlock(FabricBlockSettings.copyOf(CRIMSON_FENCE_GATE).mapColor(MapColor.PINK), ROSEWATER_WOOD_TYPE));
 
-    public static final Block ROSEWATER_DOOR = register("rosewater_door", new DoorBlock(FabricBlockSettings.copyOf(CRIMSON_DOOR).mapColor(MapColor.PINK), BlockSetType.CRIMSON));
-    public static final Block ROSEWATER_TRAPDOOR = register("rosewater_trapdoor", new TrapdoorBlock(FabricBlockSettings.copyOf(CRIMSON_TRAPDOOR).mapColor(MapColor.PINK), BlockSetType.CRIMSON));
-    public static final Block ROSEWATER_PRESSURE_PLATE = register("rosewater_pressure_plate", new PressurePlateBlock(PressurePlateBlock.ActivationRule.EVERYTHING, FabricBlockSettings.copyOf(CRIMSON_PRESSURE_PLATE).mapColor(MapColor.PINK), BlockSetType.CRIMSON));
-    public static final Block ROSEWATER_BUTTON = register("rosewater_button", new ButtonBlock(FabricBlockSettings.copyOf(CRIMSON_BUTTON).mapColor(MapColor.PINK),BlockSetType.CRIMSON, 30, true));
+    public static final Block ROSEWATER_DOOR = register("rosewater_door", new DoorBlock(FabricBlockSettings.copyOf(CRIMSON_DOOR).mapColor(MapColor.PINK), ROSEWATER_SET));
+    public static final Block ROSEWATER_TRAPDOOR = register("rosewater_trapdoor", new TrapdoorBlock(FabricBlockSettings.copyOf(CRIMSON_TRAPDOOR).mapColor(MapColor.PINK), ROSEWATER_SET));
+    public static final Block ROSEWATER_PRESSURE_PLATE = register("rosewater_pressure_plate", new PressurePlateBlock(PressurePlateBlock.ActivationRule.EVERYTHING, FabricBlockSettings.copyOf(CRIMSON_PRESSURE_PLATE).mapColor(MapColor.PINK), ROSEWATER_SET));
+    public static final Block ROSEWATER_BUTTON = register("rosewater_button", new ButtonBlock(FabricBlockSettings.copyOf(CRIMSON_BUTTON).mapColor(MapColor.PINK),ROSEWATER_SET, 30, true));
 
-    public static final Block ROSEWATER_SIGN = register("rosewater_sign", new SignBlock(FabricBlockSettings.copyOf(CRIMSON_SIGN), WoodType.CRIMSON));
-    public static final Block ROSEWATER_HANGING_SIGN = register("rosewater_hanging_sign", new HangingSignBlock(FabricBlockSettings.copyOf(CRIMSON_HANGING_SIGN), WoodType.CRIMSON));
+    public static final SignBlock ROSEWATER_SIGN = (SignBlock) register("rosewater_sign", new SignBlock(FabricBlockSettings.copyOf(CRIMSON_SIGN), ROSEWATER_WOOD_TYPE));
+    public static final HangingSignBlock ROSEWATER_HANGING_SIGN = (HangingSignBlock) register("rosewater_hanging_sign", new HangingSignBlock(FabricBlockSettings.copyOf(CRIMSON_HANGING_SIGN), ROSEWATER_WOOD_TYPE));
+
 
     private static void registerStrippable() {
         StrippableBlockRegistry.register(ROSEWATER_STEM, STRIPPED_ROSEWATER_STEM);
